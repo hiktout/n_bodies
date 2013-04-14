49a89ae4fa69c7fe2b726ffdf0518057c1c6af17
Laurence de Bruxelles <hiktout@gmail.com>
2013-04-14 19:45:44 +0100

    lots of messing around as usual

--- rnd_cluster.py
+++ rnd_cluster.py
@@ -1,11 +1,5 @@
 import numpy as np
 
-from n_bodies_fast import N_bodies
-
-from plotter3D import Plotter3D
-
-from metrics_fast import Energy
-
 def random_positions(R,N):
 	r = np.random.uniform(0,R,N)
 	theta = np.random.uniform(0,2*np.pi,N)
@@ -31,14 +25,14 @@ def random_velocities(V,N):
 
 if __name__ == '__main__':
 	M = 2e30 # about solar mass
-	R = 1e14
+	R = 7e19
 	V = 1e3
 	N = 500
 
 	day = 86400
 	year = 365*day
 
-	T = 1e5*year
+	T = 3e11*year
 
 	m = M*np.ones(N)
 	x = random_positions(R,N)
@@ -50,25 +44,36 @@ if __name__ == '__main__':
 	# x[bh] = [2*R,0,0] # BLACK HOLE
 	# v[bh] = [0,V,0] # BLACK HOLE
 
-	# split into two clusters
-	x[:N/2] += [2*R,0,0]
-	x[N/2:] += [-2*R,0,0]
+	# split into four clusters
+	# x[:N/4] += [2*R,0,0]
+	# x[N/4:N/2] += [-2*R,0,0]
+	# x[N/2:3*N/4] += [0,2*R,0]
+	# x[3*N/4:] += [0,-2*R,0]
 
 	# from dark_matter import Dark_bodies
+	from n_bodies_fast import N_bodies
 	
-	cluster = N_bodies(m,x,v,5*year,0.1*R)
+	cluster = N_bodies(m,x,v,year*1e8,softening=0.2)
 
-	from n_bodies import N_view
+	# from n_bodies import N_view
 
-	c1 = N_view(cluster,np.s_[:N/2])
-	c2 = N_view(cluster,np.s_[N/2:])
+	# c1 = N_view(cluster,np.s_[:N/4])
+	# c2 = N_view(cluster,np.s_[N/4:N/2])
+	# c3 = N_view(cluster,np.s_[N/2:3*N/4])
+	# c4 = N_view(cluster,np.s_[3*N/4:])
 
 	# BLACK HOLE
 	# bh_l = range(N)
 	# bh_l.remove(bh)
 	# c = N_view(cluster,bh_l)
 
-	# energy = Energy()
+	from metrics_fast import Energy
+	energy = Energy()
+
+	from plotter_anim import Plotter
+	data_plot = Plotter({'Energy':'red','KE':'blue','PE':'green'},
+		axis=[0,T,-4e36,2e37],position=211)
+	data_plot.show()
 
 	# cluster.find_r()
 	# V = -0.5*energy.potential_energy(cluster)/(N*np.sum(m)) # average velocity from virial
@@ -76,16 +81,13 @@ if __name__ == '__main__':
 	# v = random_velocities(V,N)
 	# cluster = N_bodies(m,x,v,year,0.1*R)
 
+	from plotter3D import Plotter3D
 	plot = Plotter3D({
 		'x':{'color':(1,1,1,0.5),'size':3.0},
 		'bh':{'color':(1,0,0,1),'size':6.0},
 		'w1':{'color':(0,1,0,0.5),'line':True},
-		'w2':{'color':(0,1,0,0.5),'line':True}
-		},4*R)
-
-	# from plotter_anim import Plotter
-	# data_plot = Plotter({'Energy':'red','KE':'blue','PE':'green'},
-	# 	axis=[0,T,-2e42,2e42],position=211)
+		# 'w2':{'color':(0,1,0,0.5),'line':True},
+	},4*R)
 
 	# from metrics import position_dist
 	# from plotter_chaco import HistPlotter
@@ -99,28 +101,31 @@ if __name__ == '__main__':
 	from coroutines import printer
 	pr = printer()
 
-	from metrics import angular_v
+	# from metrics import angular_v
+	# from metrics import find_barycentre
 
-	from metrics import find_barycentre
-
-	# data_plot.show()
+	# from density import density
+	# d = density(30,0.3*R)
 
 	try:
 		while True:
 			cluster.leapfrog()
 
-			a1 = angular_v(c1)
-			a2 = angular_v(c2)
+			# a1 = angular_v(cluster)
+			# a1 = angular_v(c1)
+			# a2 = angular_v(c2)
 
-			o = np.vstack((find_barycentre(c1),find_barycentre(c2)))
+			# o = np.vstack((find_barycentre(c1),find_barycentre(c2)))
 
 			plot.send({
 				'x':cluster.x,
 				# 'bh':cluster.x[np.newaxis,bh],
-				'bh':o,
+				# 'bh':c1.x,
 				# 'w1':a1,
-				# 'w2':a2
+				# 'w2':a2,
 			})
+			
+			# d.send(cluster.x)
 
 			# quiver.send((x[:,1],x[:,2],v[:,0:2]/V))
 
@@ -128,26 +133,26 @@ if __name__ == '__main__':
 
 			# pr.send((str(find_barycentre(cluster)),))
 
-			# energy(cluster)
-			# data_plot.send({
-			# 	'Energy':(cluster.t,energy.total),
-			# 	'KE':(cluster.t,energy.ke),
-			# 	'PE':(cluster.t,energy.pe)
-			# 	})
-			# strf = '%+.3e'
-			# pr.send(['Time (yr):', str(cluster.t/year), '|'])
-			# pr.send(['Virial:', strf % energy.virial])
-			# pr.send([
-			# 	'Time:', str(cluster.t), '|',
-			# 	'Total Energy:', strf % energy.percent_total, '|',
-			# 	'Kinetic Energy:', strf % energy.percent_ke, '|',
-			# 	'Potential Energy:', strf % energy.percent_pe,
-			# 	])
+			energy(cluster)
+			data_plot.send({
+				'Energy':(cluster.t,energy.total),
+				'KE':(cluster.t,energy.ke),
+				'PE':(cluster.t,energy.pe)
+			})
+			strf = '%+.3e'
+			pr.send(['Time (yr):', str(cluster.t/year), '|'])
+			pr.send(['Virial:', strf % energy.virial])
+			pr.send([
+				# 'Time:', str(cluster.t), '|',
+				'Total Energy:', strf % energy.total, '|',
+				'Kinetic Energy:', strf % energy.ke, '|',
+				'Potential Energy:', strf % energy.pe,
+			])
 
 			pr.send(None)
-			if cluster.t >= T:
-				print 'end'
-				break
+			# if cluster.t >= T:
+			# 	print 'end'
+			# 	break
 	except KeyboardInterrupt:
 		pass
 		# plot.close()
