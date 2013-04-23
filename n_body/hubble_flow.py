b6527e9d1a6ccaefd5c6199835b0e71ea06744fb
Laurence de Bruxelles <hiktout@gmail.com>
2013-04-23 20:22:53 +0100

    Made it work
    works in comoving coordinates, then converts to physical
    time varying scale factor included
    currently built in assumption of critical density

--- n_body/hubble_flow.py
+++ n_body/hubble_flow.py
@@ -1,24 +1,55 @@
+import numpy as np
+from util import dot
+
 from .fast import N_bodies
 
 class Hubble_Bodies(N_bodies):
-	H = 75
+	Omega0 = 1.
+	H0 = 75.
 
 	def __init__(self,m,x,v,dt,softening,initial_time=1e-9):
 		super(Hubble_Bodies,self).__init__(m,x,v,dt,softening)
 
 		self.t = initial_time
-		# assume initial positions are in comoving coordinates
-		# self.x *= self.scale_factor()
-		# self.v *= self.scale_factor()
 
-	def scale_factor(self):
-		return (self.t*self.H*3/2)**(2./3.)
+		# create comoving coordinates
+		# assume initial positions are in comoving coordinates
+		self.x_c = self.x.copy()
+		self.v_c = self.v.copy()
+		# correct physical coordinates
+		self.x *= self.scale()
+		self.v *= self.scale()
+		self.v += self.hubble()*self.x
+
+	def scale(self):
+		return (self.t*self.H0*3./2.)**(2./3.)
+
+	def hubble(self):
+		return 2./(3.*self.t)
+
+	def find_r(self):
+		self.r[...] = self.x_c[:,np.newaxis,:]-self.x_c
+		self.r_u[...] = self.r[self.i_u]
+		self.r_norm2_u[...] = dot(self.r_u,self.r_u)
+
+	def find_a(self):
+		super(Hubble_Bodies,self).find_a()
+
+		self.a += 0.5*self.Omega0*self.x_c*self.H0**2
+		self.a /= self.scale()**2
 
 	def update_v(self):
 		"""
 		Include Hubble flow in velocity, to simulate expansion
 		"""
-		self.v += self.dt/self.scale_factor()
+		self.v_c += (self.a - self.hubble()*self.v_c)*self.dt
 
 	def update_x(self):
-		self.x += self.v*self.dt/self.scale_factor()**2
+		self.x_c += self.v_c*self.dt/self.scale()
+
+	def leapfrog(self):
+		super(Hubble_Bodies,self).leapfrog()
+
+		# convert to physical coordinates
+		self.x = self.scale()*self.x_c
+		self.v = self.v_c + self.hubble()*self.x
